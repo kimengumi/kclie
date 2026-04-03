6f64b596e663831d7abcaa4198f0acde2a92536e
Antonio Rossetti <antonio@rossetti.fr>
2026-04-03 02:32:59 +0200

    add getFirstRow, code formatting

--- lib/php/kSql.lib.php
+++ lib/php/kSql.lib.php
@@ -25,84 +25,101 @@
 * Minimalistic SQL Library, mostly useful to quick write php CLI batchs.
 */
 
-class kSql {
+class kSql
+{
 
-	/**
-	 * @var mysqli null
-	 */
-	private static $db = null;
+    /**
+     * @var mysqli null
+     */
+    private static $db = null;
 
-	/**
-	 * Connect to Mysql
-	 */
-	public static function init() {
-		//@todo manage .my.cnf
-		self::$db = new mysqli( KCLIE_KSQL_HOST, KCLIE_KSQL_USER, KCLIE_KSQL_PASS, KCLIE_KSQL_DB );
-	}
+    /**
+     * Connect to Mysql
+     */
+    public static function init()
+    {
+        //@todo manage .my.cnf
+        self::$db = new mysqli(KCLIE_KSQL_HOST, KCLIE_KSQL_USER, KCLIE_KSQL_PASS, KCLIE_KSQL_DB);
+    }
 
-	/**
-	 * @param $query
-	 *
-	 * @return mixed|bool
-	 */
-	public static function getFirst( $query ) {
+    /**
+     * Get first value of first row of query result
+     * @param $query
+     * @return false|mixed|mysqli_result|void
+     */
+    public static function getFirst($query)
+    {
+        if (!self::$db) {
+            self::init();
+        }
 
-		if ( ! self::$db ) {
-			self::init();
-		}
+        /** @var \mysqli_result $res */
+        if (is_bool($res = self::$db->query($query))) {
+            return $res ?? die("SQL ERR " . self::$db->error . "\n" . $query . "\n");
+        } else {
+            while ($row = $res->fetch_array()) {
+                foreach ($row as $value) {
+                    return $value;
+                }
+            }
+        }
+        return false;
+    }
 
-		/** @var \mysqli_result $res */
-		if ( is_bool( $res = self::$db->query( $query ) ) ) {
-			return $res ?? die( "SQL ERR " . self::$db->error . "\n" . $query . "\n" );
-		} else {
-			while ( $row = $res->fetch_array() ) {
-				foreach ( $row as $value ) {
-					return $value;
-				}
-			}
-		}
+    /**
+     * Get first row of query result
+     * @param $query
+     * @return array|false|mysqli_result|void|null
+     */
+    public static function getFirstRow($query)
+    {
+        if (!self::$db) {
+            self::init();
+        }
 
-		return false;
-	}
+        /** @var \mysqli_result $res */
+        if (is_bool($res = self::$db->query($query))) {
+            return $res ?? die("SQL ERR " . self::$db->error . "\n" . $query . "\n");
+        } else {
+            return $res->fetch_array(MYSQLI_ASSOC);
+        }
+    }
 
-	/**
-	 * @param $query
-	 *
-	 * @return array|bool
-	 */
-	public static function getAll( $query ) {
+    /**
+     * Get all rows of query result
+     * @param $query
+     * @return array|mysqli_result|void
+     */
+    public static function getAll($query)
+    {
+        if (!self::$db) {
+            self::init();
+        }
 
-		if ( ! self::$db ) {
-			self::init();
-		}
-
-		/** @var \mysqli_result $res */
-		if ( is_bool( $res = self::$db->query( $query ) ) ) {
-			return $res ?? die( "SQL ERR " . self::$db->error . "\n" . $query . "\n" );
-		} else {
-			return $res->fetch_all( MYSQLI_ASSOC );
-		}
-
-
-	}
+        /** @var \mysqli_result $res */
+        if (is_bool($res = self::$db->query($query))) {
+            return $res ?? die("SQL ERR " . self::$db->error . "\n" . $query . "\n");
+        } else {
+            return $res->fetch_all(MYSQLI_ASSOC);
+        }
+    }
 
 
-	/**
-	 * @param $query
-	 *
-	 * @return bool|mixed
-	 */
-	public static function write( $query ) {
+    /**
+     * For write operations / queries
+     * @param $query
+     * @return true|void
+     */
+    public static function write($query)
+    {
+        if (!self::$db) {
+            self::init();
+        }
 
-		if ( ! self::$db ) {
-			self::init();
-		}
-
-		/** @var mysqli_result $res */
-		if ( ! self::$db->query( $query ) ) {
-			die( "SQL ERR " . self::$db->error . "\n" . $query . "\n" );
-		}
-
-		return true;
-	}
+        /** @var mysqli_result $res */
+        if (!self::$db->query($query)) {
+            die("SQL ERR " . self::$db->error . "\n" . $query . "\n");
+        }
+        return true;
+    }
 }
